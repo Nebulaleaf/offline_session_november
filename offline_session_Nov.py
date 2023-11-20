5d4535f4d031e5400b4e699d7da9f46bea1868d5
Alexandra Falk-Schmidtberger <nebulaleaf@gmail.com>
2023-11-20 11:51:15 +0100

    finished exercises

--- offline_session_Nov.py
+++ offline_session_Nov.py
@@ -44,5 +44,88 @@ def count_num():
 
 count_num()
 
+# Write a function which iterates the integers from 0 to 50. For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz". 
+# Hint: Remember the modulo operator from first homework? 
+# Sample Output : 
+# fizzbuzz 
+# 1 
+# 2 
+# fizz 
+# 4 
+# buzz
+
+def fizzbuzz():
+    for i in range(0, 51):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 5 == 0:
+            print("buzz")
+        elif  i % 3 == 0:
+            print("fizz")
+        else:
+            print(i)
+
+fizzbuzz()
+
+#Write a function to calculate a dog's age in dog's years. 
+#Note : For the first two years, a dog year is equal to 10.5 human years. After that, each dog year equals 4 human years. 
+#Expected Output : 
+#Input a dog's age in human years: 15 
+#The dog's age in dog's years is 73
+# first year = 10.5
+# second year = 10.5
+# every other year = 4
 
 
+def dogsyear (h_age):
+    if h_age <= 0:
+        print("It's not even born yet! Please provide valid age")
+    elif h_age == 1:
+        d_age = 10.5
+        print(f"Your dog is {d_age} old.")
+    elif h_age == 2:
+        d_age = 10.5 * 2
+        print(f"Your dog is {d_age} old.")
+    else:
+        d_age = 10.5 * 2 + (h_age - 2) * 4
+        print(f"Your dog is {d_age} old.")
+
+h_age = int(input("What's the age of the dog in human years?\n"))
+
+dogsyear(h_age)
+
+# Write a function to construct the following pattern, using a nested loop. 
+# Expected Output : 
+# 1 
+# 22 
+# 333 
+# 4444 
+# 55555 
+# 666666 
+# 7777777 
+# 88888888 
+# 999999999
+
+def pyramide():
+    for i in range(1, 10):  # Outer loop
+        for j in range(1, 10):  # Inner loop
+            print(i, end='')
+            if i <= j:
+                break
+        print() #honestly, this whole exercise was just trial and error with the exercise of session 4 (?), I don't "get it" but it works -.-
+
+pyramide()
+
+# Write a function which removes characters from a string which are an odd index
+
+def remove_odd_index_chars(input_string):
+    result = ""
+    for i in range(len(input_string)):
+        if i % 2 == 0:
+            result += input_string[i]
+    return result
+
+input_string = "Whatever!"
+result = remove_odd_index_chars(input_string)
+print(result)
+#this I couldnt figure out myself, the part with "for i in range" I did but the whole function to work I couldnt manage
